9e74de808ec351342a4c7e22cf145681455705b8
Stefany <stefanypas123@hotmail.com>
2025-06-25 21:36:02 -0300

    Alterações design login

--- app/resources/scripts.sql
+++ app/resources/scripts.sql
@@ -1,9 +1,7 @@
-create table controle_transacao (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	descricao VARCHAR(100) NOT NULL,
-	valor DECIMAL(10,2) NOT NULL,
-	tipo ENUM('ENTRADA', 'SAIDA') NOT NULL,
-	data_inclusao DATETIME NOT NULL 
+CREATE TABLE controle_transacao (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descricao VARCHAR(100) NOT NULL,
+  valor DECIMAL(10,2) NOT NULL,
+  tipo ENUM('ENTRADA', 'SAIDA') NOT NULL,
+  data_inclusao DATE NOT NULL
 );
-ALTER TABLE controle_transacao
-MODIFY COLUMN data_inclusao DATEBASE;
